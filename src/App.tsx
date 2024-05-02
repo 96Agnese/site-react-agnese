f5549fca810174a17ff74011c7d4f070466c9111
Agnese <agnese.morlin@rawfish.com>
2024-05-02 15:34:21 +0200

    ✨ Add header and screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Homepage from "./screen/Homepage/Homepage";
+import { Route, Routes } from "react-router-dom";
+import Project from "./screen/Project/Project";
+import Contact from "./screen/Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/screen/Project/Project.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <div>
+      <p>SCREEN PROGETTI</p>
+      <p>SCREEN PROGETTI</p>
+      <p>SCREEN PROGETTI</p>
+      <p>SCREEN PROGETTI</p>
+      <p>SCREEN PROGETTI</p>
+      <p>SCREEN PROGETTI</p>
+      <p>SCREEN PROGETTI</p>
+    </div>
+  );
+};
+
+export default Project;
